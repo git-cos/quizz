61f0738aadc8fdfa905220d3be2411feddf5e8c2
git-cos <cossettini.francesco@gmail.com>
2025-10-04 19:38:08 +0200

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,86 @@
+var nomeGiocatore = window.prompt("Ciao, inserisci il tuo nome:");
+console.log("il valore di titolo è: " + document.getElementById("titolo").innerHTML);
+
+var scelta1;
+function click3 () {
+    scelta1 = "3";
+    alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta sbagliata😕");
+}
+function click4 () {
+    scelta1 = "4";
+    alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta giusta😎");
+}
+function click5 () {
+    scelta1 = "5";
+    alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta sbagliata😕");
+}
+var scelta2;
+function clickGiacarta () {
+    scelta2 = "Giacarta";
+    alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta sbagliata😕");
+}
+function clickBerlino () {
+    scelta2 = "Berlino";
+    alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta sbagliata😕");
+}
+function clickNuova () {
+    scelta2 = "Nuova Dehli";
+    alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta giusta😎");
+}
+var scelta3;
+function clickKim () {
+    scelta3 = "Kim Jong-un";
+    alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta giusta😎");
+}
+function clickXi () {
+    scelta3 = "Xi Jinping";
+    alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta sbagliata😕");
+}
+function clickSergio () {
+    scelta3 = "Sergio Mattarella";
+    alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta sbagliata😕");
+}
+var scelta4;
+function click1787 () {
+    scelta4 = "1787";
+    alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta sbagliata😕");
+}
+function click1788 () {
+    scelta4 = "1788";
+    alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta sbagliata😕");
+}
+function click1789 () {
+    scelta4 = "1789";
+    alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta giusta😎");
+}
+var scelta5;
+function clickOesis () {
+    scelta5 = "Oesis";
+    alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta sbagliata😕");
+}
+function clickBlur () {
+    scelta5 = "Blur";
+    alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta giusta😎");
+}
+function clickRush () {
+    scelta5 = "Rush";
+    alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta sbagliata😕");
+}
+var scelta6;
+function click206 () {
+    scelta6 = "206";
+    alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta giusta😎");
+}
+function click205 () {
+    scelta6 = "205";
+    alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta sbagliata😕");
+}
+function click204 () {
+    scelta6 = "204";
+    alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta sbagliata😕");
+}
+var parere1;
+function clickparere () {
+    parere1 = "parere";
+    alert ("Ottima scelta😉");
+}
