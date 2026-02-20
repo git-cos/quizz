e3d00c92a1b44718384ade7d49803bcf50e90243
git-cos <cossettini.francesco@gmail.com>
2026-02-20 18:53:51 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,83 +1,174 @@
-var nomeGiocatore = window.prompt("Ciao, inserisci il tuo nome:");
+document.getElementById("start").style.display="block";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="none";
+var nomeGiocatore = document.getElementById('username').value;
 console.log("il valore di titolo è: " + document.getElementById("titolo").innerHTML);
+
+function start(){
+    document.getElementById("start").style.display="none";
+    document.getElementById("lebron").style.display="block";
+}
 
 var scelta1;
 function click3 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta1 = "3";
     alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta sbagliata😕");
 }
 function click4 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta1 = "4";
     alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta giusta😎");
 }
 function click5 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta1 = "5";
     alert (nomeGiocatore + ", hai scelto " + scelta1 + " e hai fatto la scelta sbagliata😕");
 }
+function passaindia(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="block";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="none";
+}
 var scelta2;
 function clickGiacarta () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta2 = "Giacarta";
     alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta sbagliata😕");
 }
 function clickBerlino () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta2 = "Berlino";
     alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta sbagliata😕");
 }
 function clickNuova () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta2 = "Nuova Dehli";
     alert (nomeGiocatore + ", hai scelto " + scelta2 + " e hai fatto la scelta giusta😎");
 }
+function passacorea(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="block";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="none";
+}
 var scelta3;
 function clickKim () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta3 = "Kim Jong-un";
     alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta giusta😎");
 }
 function clickXi () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta3 = "Xi Jinping";
     alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta sbagliata😕");
 }
 function clickSergio () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta3 = "Sergio Mattarella";
     alert (nomeGiocatore + ", hai scelto " + scelta3 + " e hai fatto la scelta sbagliata😕");
 }
+function passabastiglia(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="block";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="none";
+}
 var scelta4;
 function click1787 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta4 = "1787";
     alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta sbagliata😕");
 }
 function click1788 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta4 = "1788";
     alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta sbagliata😕");
 }
 function click1789 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta4 = "1789";
     alert (nomeGiocatore + ", hai scelto " + scelta4 + " e hai fatto la scelta giusta😎");
 }
+function passasong2(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="block";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="none";
+}
 var scelta5;
 function clickOesis () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta5 = "Oesis";
     alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta sbagliata😕");
 }
 function clickBlur () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta5 = "Blur";
     alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta giusta😎");
 }
 function clickRush () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta5 = "Rush";
     alert (nomeGiocatore + ", hai scelto " + scelta5 + " e hai fatto la scelta sbagliata😕");
 }
+function passabastiglia(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="block";
+document.getElementById("fine").style.display="none";
+}
 var scelta6;
 function click206 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta6 = "206";
     alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta giusta😎");
 }
 function click205 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta6 = "205";
     alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta sbagliata😕");
 }
 function click204 () {
+    nomeGiocatore = document.getElementById('username').value;
     scelta6 = "204";
     alert (nomeGiocatore + ", hai scelto " + scelta6 + " e hai fatto la scelta sbagliata😕");
+}
+function passafine(){
+    document.getElementById("start").style.display="none";
+document.getElementById("lebron").style.display="none";
+document.getElementById("india").style.display="none";
+document.getElementById("corea").style.display="none";
+document.getElementById("bastiglia").style.display="none";
+document.getElementById("song2").style.display="none";
+document.getElementById("ossa").style.display="none";
+document.getElementById("fine").style.display="block";
 }
 var parere1;
 function clickparere () {
